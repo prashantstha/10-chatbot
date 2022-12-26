0b7692567b87ef7d1cfdf615ffdee748dacd8a64
prashantstha <prashant.stha@hotmail.com>
2022-12-26 08:20:06 +0545

    server url changed

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch the data from server 
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://prashant-chat-bot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
